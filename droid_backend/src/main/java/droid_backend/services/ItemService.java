3099ae060c467afe138fee5554964ad7f8437450
Siraaj Wilkinson <siraaj.wilkonson1995@gmail.com>
2016-08-30 22:59:21 +0200

    updated

--- droid_backend/src/main/java/droid_backend/services/ItemService.java
+++
@@ -1,9 +0,0 @@
-package droid_backend.services;
-
-import droid_backend.domain.shop.Item;
-
-/**
- * Created by Siraaj on 08/17/2016.
- */
-public interface ItemService extends Service<Item,Long> {
-}
--- droid_backend/src/main/java/droid_backend/services/SalesService.java
+++
@@ -1,9 +0,0 @@
-package droid_backend.services;
-
-import droid_backend.domain.shop.ShopProcess;
-
-/**
- * Created by Siraaj on 08/18/2016.
- */
-public interface SalesService extends Service<ShopProcess,Long> {
-}
