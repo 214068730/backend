62c63cc482a3e561230839314abec928b29ffe43
Siraaj Wilkinson <siraaj.wilkonson1995@gmail.com>
2016-08-31 00:28:50 +0200

    updateFinal

--- src/main/java/droid_backend/services/OrderLineService.java
+++
@@ -1,9 +0,0 @@
-package droid_backend.services;
-
-import droid_backend.domain.shop.Orderline;
-
-/**
- * Created by Siraaj on 08/19/2016.
- */
-public interface OrderLineService extends Service<Orderline,Long> {
-}
