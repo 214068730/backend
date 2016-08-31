df39521f3556751b1526cd39b99fe0ec3b9ef326
Siraaj Wilkinson <siraaj.wilkonson1995@gmail.com>
2016-09-01 01:03:43 +0200

    openshift ready

--- src/main/java/droid_backend/repository/CustomerRepository.java
+++ src/main/java/droid_backend/repository/CustomerRepository.java
@@ -1,9 +1,8 @@
 package droid_backend.repository;
 
 import droid_backend.domain.customer.Customer;
-import droid_backend.domain.shop.Item;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+
 
 /**
  * Created by Siraaj on 08/03/2016.
@@ -11,6 +10,6 @@ import org.springframework.stereotype.Repository;
 
 
 
-@Repository
+
 public interface CustomerRepository  extends CrudRepository<Customer,Long>  {
 }
