e4aab18156d53549116455c75f48c91c3685c3b5
John <John@co.za>
2016-09-03 07:33:00 +0200

    updated

--- backend/src/main/java/droid_backend/domain/customer/IComment.java
+++
@@ -1,8 +0,0 @@
-package droid_backend.domain.customer;
-
-/**
- * Created by Siraaj on 08/03/2016.
- */
-public interface IComment {
-    public void comment(String comment);
-}
--- backend/src/main/java/droid_backend/domain/customer/IRequestItem.java
+++
@@ -1,8 +0,0 @@
-package droid_backend.domain.customer;
-
-/**
- * Created by Siraaj on 08/03/2016.
- */
-public interface IRequestItem {
-    public void RequestItem(String requestItem);
-}
